a3b6bd907030d3ff51a7235896a36a19dda8d783
laitingyou <laitingyou@outlook.com>
2019-08-23 20:01:32 +0800

    qq

---
+++ blocks/Zaza/src/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="comtainer">
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: "helloWorld",
+  data() {
+    return {
+      msg: "helloWorld"
+    };
+  },
+  methods: {}
+};
+</script>
+<style>
+.comtainer {
+  text-algin: center;
+}
+</style>
